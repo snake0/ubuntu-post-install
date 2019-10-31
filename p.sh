da8e95a8db0d5d8c60ac58aef1a95a90544615e1
Nathan Jia <jiaxg1998@sjtu.edu.cn>
2019-11-01 00:29:26 +0800

    add p.sh

--- p.sh
+++ p.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install zsh git tree cloc valgrind gdb htop net-tools vim curl wget
+sudo apt install zsh git tree cloc valgrind gdb htop net-tools vim curl wget strace meld p7zip
 sudo apt install build-essential bison flex cmake qemu-system-x86 qemu-system-arm ghc libghc-src-exts-dev libghc-ghc-paths-dev libghc-parsec3-dev libghc-random-dev libghc-ghc-mtl-dev libghc-src-exts-dev libghc-async-dev gcc-arm-linux-gnueabi g++-arm-linux-gnueabi libgmp3-dev cabal-install curl freebsd-glue libelf-freebsd-dev libusb-1.0-0-dev qemu-utils gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 cabal update
 cabal install bytestring-trie
